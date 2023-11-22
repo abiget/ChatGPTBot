ff0fae3060aa588d022fe7f0f79c2bc6f005dd39
abiget <getachewante@gmail.com>
2023-11-22 10:49:40 +0300

    add streaming

--- app/Http/Controllers/ChatBotController.php
+++ app/Http/Controllers/ChatBotController.php
@@ -4,25 +4,49 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use OpenAI;
 
 class ChatBotController extends Controller
 {
     public function index()
     {
-        if (request()->ajax()) {
-            $question = request('question');
-            
-            $reponse = Http::withHeaders([
-                'Authorization' => 'Bearer ' . env('OPENAP_API_KEY'),
-            ])->post('https://api.openai.com/v1/chat/completions', [
+        $question = request()->query('question');
+
+        $client = OpenAI::client(env('OPENAP_API_KEY'));
+
+        return response()->stream(function () use ($question, $client) {
+            $stream = $client->chat()->createStreamed([
                 "model" => "gpt-3.5-turbo",
-                "messages" => [["role" => "user", "content" => "$question"]],
-                "temperature" => 0.7
+                "messages" => [
+                    ["role" => "user", "content" => "$question"]
+                ],
+                'max_tokens' => 1024,
             ]);
 
-            $chat_response = $reponse->json()['choices'][0]['message']['content'];
+            foreach ($stream as $response) {
+                $text = $response->choices[0]->toArray()['delta']['content'];
+                
+                if (connection_aborted()) {
+                    break;
+                }
 
-            return response()->json(['message' => $chat_response]);
-        }
+                echo "event: update\n";
+                echo 'data: ' . $text;
+                echo "\n\n";
+                ob_flush();
+                flush();
+            }
+
+            echo "event: update\n";
+            echo 'data: <end>';
+            echo "\n\n";
+            ob_flush();
+            flush();
+        }, 200, [
+            'Cache-Control' => 'no-cache',
+            'X-Accel-Buffering' => 'no',
+            'Content-Type' => 'text/event-stream',
+        ]);
+
     }
 }
